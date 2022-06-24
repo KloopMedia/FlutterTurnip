72c200a4195c7584d9997726c29986000fa797c0
atna <atay.narynov@gmail.com>
2022-06-24 13:07:47 +0600

    Fix defect merge

--- lib/src/features/tasks/features/list_tasks/cubit/tasks_cubit.dart
+++ lib/src/features/tasks/features/list_tasks/cubit/tasks_cubit.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gigaturnip/src/features/tasks/constants/status.dart';
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
-import 'package:gigaturnip/extensions/buildcontext/loc.dart';
 
 part 'tasks_state.dart';
 
