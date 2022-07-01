3ab95a3e5dd7c727c4005ac3e3ad16e7cecd97c8
atna <atay.narynov@gmail.com>
2022-07-01 11:12:11 +0600

    Fix api routes

--- packages/gigaturnip_api/lib/src/helpers/routes.dart
+++ packages/gigaturnip_api/lib/src/helpers/routes.dart
@@ -1,25 +1,25 @@
 // Campaigns routes
-const campaignsRoute = '/api/v1/campaigns';
-const userCampaignsRoute = '$campaignsRoute/list_user_campaigns';
-const selectableCampaignsRoute = '$campaignsRoute/list_user_selectable';
+const campaignsRoute = '/api/v1/campaigns/';
+const userCampaignsRoute = '$campaignsRoute/list_user_campaigns/';
+const selectableCampaignsRoute = '$campaignsRoute/list_user_selectable/';
 
 // TaskStages routes
-const taskStagesRoute = '/api/v1/taskstages';
-const userRelevantTaskStageRoute = '$taskStagesRoute/user_relevant';
+const taskStagesRoute = '/api/v1/taskstages/';
+const userRelevantTaskStageRoute = '$taskStagesRoute/user_relevant/';
 
 // Tasks routes
-const tasksRoute = '/api/v1/tasks';
-const selectableTasksRoute = '$tasksRoute/user_selectable';
-const relevantTasksRoute = '$tasksRoute/user_relevant';
+const tasksRoute = '/api/v1/tasks/';
+const selectableTasksRoute = '$tasksRoute/user_selectable/';
+const relevantTasksRoute = '$tasksRoute/user_relevant/';
 
 // Specific task's routes
-const integratedTasksActionRoute = '/get_integrated_tasks';
-const displayedPreviousTasksActionRoute = '/list_displayed_previous';
-const openPreviousTaskActionRoute = '/open_previous';
-const releaseTaskActionRoute = '/release_assignment';
-const requestTaskActionRoute = '/request_assignment';
-const triggerWebhookActionRoute = '/trigger_webhook';
-const reopenTaskActionRoute = '/uncomplete';
+const integratedTasksActionRoute = '/get_integrated_tasks/';
+const displayedPreviousTasksActionRoute = '/list_displayed_previous/';
+const openPreviousTaskActionRoute = '/open_previous/';
+const releaseTaskActionRoute = '/release_assignment/';
+const requestTaskActionRoute = '/request_assignment/';
+const triggerWebhookActionRoute = '/trigger_webhook/';
+const reopenTaskActionRoute = '/uncomplete/';
 
 
 // Notifications routes
