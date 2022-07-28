4393e4e0c2f95a09104a2c89a2ea9f1cfa0cd684
Rambo <ramilsalihar@gmail.com>
2022-07-28 18:09:16 +0600

    <refactor> Updating the localization

--- lib/src/features/tasks/features/list_tasks/view/tasks_list_view.dart
+++ lib/src/features/tasks/features/list_tasks/view/tasks_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gigaturnip/src/widgets/cards/id_title_card.dart';
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
+import 'package:gigaturnip/extensions/buildcontext/loc.dart';
 
 typedef ItemCallback = void Function(Task item);
 typedef RefreshCallback = void Function();
@@ -29,7 +30,11 @@ class TasksListView extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text('To-do', style: Theme.of(context).textTheme.titleMedium, textAlign: TextAlign.start,),
+              child: Text(
+                context.loc.todo,
+                style: Theme.of(context).textTheme.titleMedium,
+                textAlign: TextAlign.start,
+              ),
             ),
             Expanded(
               child: ListView.builder(
@@ -52,7 +57,10 @@ class TasksListView extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text('Done', style: Theme.of(context).textTheme.titleMedium,),
+              child: Text(
+                context.loc.done,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
             ),
             Expanded(
               child: ListView.builder(
@@ -79,4 +87,3 @@ class TasksListView extends StatelessWidget {
     );
   }
 }
-
