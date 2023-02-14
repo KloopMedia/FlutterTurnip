f2f9b6e466a593bcefa1b0c5fce1956f236b1804
atna <atay.narynov@gmail.com>
2023-02-14 18:28:10 +0600

    Add translation for form divider, and join campaign dialog.

--- lib/src/utilities/dialogs/join_campaign_dialog.dart
+++ lib/src/utilities/dialogs/join_campaign_dialog.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:gigaturnip/extensions/buildcontext/loc.dart';
 import 'package:gigaturnip/src/utilities/dialogs/generic_dialog.dart';
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 
 Future<bool> joinCampaignDialog(BuildContext context, Campaign campaign) {
   return showGenericDialog<bool>(
     context: context,
-    title: 'Join campaign?',
-    content: campaign.description,
+    title: campaign.description,
+    content: context.loc.join_campaign,
     optionsBuilder: () => {
-      'no': false,
-      'yes': true,
+      context.loc.no: false,
+      context.loc.yes: true,
     },
   ).then(
     (value) => value ?? false,
