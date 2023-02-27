46d0c220d664917c18c79dd04882d71ec8b44a75
atna <atay.narynov@gmail.com>
2023-02-27 21:24:02 +0600

    Reverse prev commit, because it is hard to control states in single widget.

--- lib/src/features/task/view/available_task_page.dart
+++ lib/src/features/task/view/available_task_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gigaturnip/src/helpers/helpers.dart';
 import 'package:gigaturnip/src/utilities/constants.dart';
-import 'package:gigaturnip_api/gigaturnip_api.dart';
+import 'package:gigaturnip/src/utilities/remote_data_type.dart';
+import 'package:gigaturnip_api/gigaturnip_api.dart' as api;
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 import 'package:go_router/go_router.dart';
 
 import '../bloc/bloc.dart';
-import 'task_list_view.dart';
 
 class AvailableTaskPage extends StatelessWidget {
   final int campaignId;
@@ -20,7 +21,7 @@ class AvailableTaskPage extends StatelessWidget {
         BlocProvider<AvailableTaskBloc>(
           create: (context) => AvailableTaskBloc(
             AvailableTaskRepository(
-              gigaTurnipApiClient: context.read<GigaTurnipApiClient>(),
+              gigaTurnipApiClient: context.read<api.GigaTurnipApiClient>(),
               campaignId: campaignId,
             ),
           ),
@@ -50,13 +51,68 @@ class TaskView extends StatelessWidget {
           );
         }
       },
-      child: TaskListView(
-        bloc: context.read<AvailableTaskBloc>(),
-        header: const Text('Available tasks'),
-        onTap: (task) {
-          context.read<AvailableTaskBloc>().add(RequestAvailableTaskAssignment(task));
-        },
+      child: SingleChildScrollView(
+        child: AvailableTaskListView(
+          bloc: context.read<AvailableTaskBloc>(),
+          header: const Text('Available tasks'),
+          onTap: (task) {
+            context.read<AvailableTaskBloc>().add(RequestAvailableTaskAssignment(task));
+          },
+        ),
       ),
+    );
+  }
+}
+
+class AvailableTaskListView extends StatelessWidget {
+  final AvailableTaskBloc bloc;
+  final Text header;
+  final void Function(Task task) onTap;
+
+  const AvailableTaskListView(
+      {Key? key, required this.bloc, required this.header, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        header,
+        BlocBuilder(
+          bloc: bloc,
+          builder: (context, state) {
+            if (state is RemoteDataFetching) {
+              return const CircularProgressIndicator();
+            }
+            if (state is AvailableTaskLoaded) {
+              return ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final task = state.data[index];
+                  return ListTile(
+                    title: Text(task.name),
+                    subtitle: Text('ID: ${task.id} ${task.complete ? 'closed' : 'open'}'),
+                    onTap: () => onTap(task),
+                  );
+                },
+                itemCount: state.data.length,
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+        BlocBuilder(
+          bloc: bloc,
+          builder: (context, state) {
+            return Pagination(
+              currentPage: state is AvailableTaskInitialized ? state.currentPage : 0,
+              total: state is AvailableTaskInitialized ? state.total : 0,
+              onChanged: (page) => bloc.add(RefetchAvailableTaskData(page)),
+              enabled: state is! RemoteDataFetching,
+            );
+          },
+        )
+      ],
     );
   }
 }
--- lib/src/features/task/view/relevant_task_page.dart
+++ lib/src/features/task/view/relevant_task_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gigaturnip/src/helpers/helpers.dart';
 import 'package:gigaturnip/src/utilities/constants.dart';
-import 'package:gigaturnip_api/gigaturnip_api.dart';
+import 'package:gigaturnip/src/utilities/remote_data_type.dart';
+import 'package:gigaturnip_api/gigaturnip_api.dart' as api;
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 import 'package:go_router/go_router.dart';
 
 import '../bloc/bloc.dart';
-import 'task_list_view.dart';
 
 class RelevantTaskPage extends StatelessWidget {
   final int campaignId;
@@ -20,7 +21,7 @@ class RelevantTaskPage extends StatelessWidget {
         BlocProvider<OpenTaskBloc>(
           create: (context) => RelevantTaskBloc(
             OpenTaskRepository(
-              gigaTurnipApiClient: context.read<GigaTurnipApiClient>(),
+              gigaTurnipApiClient: context.read<api.GigaTurnipApiClient>(),
               campaignId: campaignId,
             ),
           ),
@@ -28,7 +29,7 @@ class RelevantTaskPage extends StatelessWidget {
         BlocProvider<ClosedTaskBloc>(
           create: (context) => RelevantTaskBloc(
             ClosedTaskRepository(
-              gigaTurnipApiClient: context.read<GigaTurnipApiClient>(),
+              gigaTurnipApiClient: context.read<api.GigaTurnipApiClient>(),
               campaignId: campaignId,
             ),
           ),
@@ -61,14 +62,14 @@ class TaskView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          TaskListView(
+          RelevantTaskListView(
             bloc: context.read<OpenTaskBloc>(),
             header: const Text('Open tasks'),
             onTap: (task) {
               redirect(context, task.id);
             },
           ),
-          TaskListView(
+          RelevantTaskListView(
             bloc: context.read<ClosedTaskBloc>(),
             header: const Text('Closed tasks'),
             onTap: (task) {
@@ -77,6 +78,59 @@ class TaskView extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class RelevantTaskListView<TaskBloc extends Bloc> extends StatelessWidget {
+  final TaskBloc bloc;
+  final Text header;
+  final void Function(Task task) onTap;
+
+  const RelevantTaskListView(
+      {Key? key, required this.bloc, required this.header, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        header,
+        BlocBuilder(
+          bloc: bloc,
+          builder: (context, state) {
+            if (state is RemoteDataFetching) {
+              return const CircularProgressIndicator();
+            }
+            if (state is RelevantTaskLoaded) {
+              return ListView.builder(
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  final task = state.data[index];
+                  return ListTile(
+                    title: Text(task.name),
+                    subtitle: Text(task.complete ? 'closed' : 'open'),
+                    onTap: () => onTap(task),
+                  );
+                },
+                itemCount: state.data.length,
+              );
+            }
+            return const SizedBox.shrink();
+          },
+        ),
+        BlocBuilder(
+          bloc: bloc,
+          builder: (context, state) {
+            return Pagination(
+              currentPage: state is RelevantTaskInitialized ? state.currentPage : 0,
+              total: state is RelevantTaskInitialized ? state.total : 0,
+              onChanged: (page) => bloc.add(RefetchRelevantTaskData(page)),
+              enabled: state is! RemoteDataFetching,
+            );
+          },
+        )
+      ],
     );
   }
 }
