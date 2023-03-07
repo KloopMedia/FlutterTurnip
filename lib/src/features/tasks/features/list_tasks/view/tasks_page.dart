6e107af73b59048794b4a1d3081cd91dde03cb0b
atna <atay.narynov@gmail.com>
2023-03-07 15:43:54 +0600

    Refactor to return task list on error.

--- lib/src/features/tasks/features/list_tasks/view/tasks_page.dart
+++ lib/src/features/tasks/features/list_tasks/view/tasks_page.dart
@@ -6,7 +6,6 @@ import 'package:gigaturnip/src/features/tasks/features/list_tasks/cubit/index.da
 import 'package:gigaturnip/src/features/tasks/features/list_tasks/view/combined_task_view.dart';
 import 'package:gigaturnip/src/features/tasks/features/list_tasks/view/index.dart';
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../view_task/view/task_page.dart';
 
@@ -16,43 +15,37 @@ class TasksPage extends StatelessWidget {
   final bool shouldJoinCampaign;
   final int? createTaskId;
 
-  const TasksPage({
-    Key? key,
-    this.campaignId,
-    this.simpleViewMode = false,
-    this.shouldJoinCampaign = false,
-    this.createTaskId
-
-  }) : super(key: key);
+  const TasksPage(
+      {Key? key,
+      this.campaignId,
+      this.simpleViewMode = false,
+      this.shouldJoinCampaign = false,
+      this.createTaskId})
+      : super(key: key);
 
   static Page page() => const MaterialPage<void>(child: TasksPage());
 
-  Future<void> joinCampaign(BuildContext context, int id) async {
-    await context.read<GigaTurnipRepository>().joinCampaign(id);
-  }
-
-
   Future<dynamic> loadCampaign(BuildContext context) async {
     if (campaignId != null) {
-      if (shouldJoinCampaign) {
-        joinCampaign(context, campaignId!);
-      }
-      if(createTaskId != null){
-        final newTask = await context.read<GigaTurnipRepository>().createTask(createTaskId!);
-
-        var location = Router.of(context).routeInformationProvider?.value.location;
-        var queryStartIndex = location?.indexOf('?') ?? -1;
-        String query;
-        if (queryStartIndex > 0) {
-          query = location?.substring(queryStartIndex) ?? '';
-        } else {
-          query = '';
-        }
-        // context.go('/campaign/${campaignId}/tasks/${newTask.id}$query');
-        return newTask.id;
-      }
       final appBloc = context.read<AppBloc>();
-      final campaign = await context.read<GigaTurnipRepository>().getCampaignById(campaignId!);
+      final repository = context.read<GigaTurnipRepository>();
+
+      if (shouldJoinCampaign) {
+        await context.read<GigaTurnipRepository>().joinCampaign(campaignId!);
+      }
+
+      if (createTaskId != null) {
+        try {
+          await Future.delayed(const Duration(seconds: 1));
+          final newTask = await repository.createTask(createTaskId!);
+
+          return newTask.id;
+        } catch (e) {
+          print(e);
+        }
+      }
+
+      final campaign = await repository.getCampaignById(campaignId!);
       appBloc.add(AppSelectedCampaignChanged(campaign));
       return campaign;
     } else {
@@ -70,7 +63,7 @@ class TasksPage extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         }
-        if(snapshot.data != null && snapshot.data is Campaign){
+        if (snapshot.data != null && snapshot.data is Campaign) {
           return MultiBlocProvider(
             providers: [
               BlocProvider<TasksCubit>(
