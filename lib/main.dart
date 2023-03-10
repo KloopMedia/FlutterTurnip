1224ff59207361dcf218c142b01fdbd61ecc345d
atna <atay.narynov@gmail.com>
2023-03-10 15:06:40 +0600

    Remove microphone permission request.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gigaturnip/firebase_options.dart';
 import 'package:gigaturnip/src/features/app/app.dart';
-import 'package:gigaturnip_repository/gigaturnip_repository.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:gigaturnip/src/features/app/routes/app_router.dart';
+import 'package:gigaturnip_repository/gigaturnip_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +19,6 @@ Future<void> main() async {
   final router = AppRouter(authenticationRepository).router;
 
   if (!kIsWeb) {
-    await Permission.microphone.request();
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   }
 
