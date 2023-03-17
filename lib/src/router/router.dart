58ebcafb387ab41669b25d479a7e47ecd73e448f
atna <atay.narynov@gmail.com>
2023-03-17 19:37:43 +0600

    Parse join campaign url query param.

--- lib/src/router/router.dart
+++ lib/src/router/router.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gigaturnip/src/router/routes/routes.dart';
+import 'package:gigaturnip_api/gigaturnip_api.dart';
 import 'package:go_router/go_router.dart';
 
 class AppRouter {
@@ -14,10 +16,78 @@ class AppRouter {
   final _taskPageShellNavigatorKey = GlobalKey<NavigatorState>();
   final _notificationPageShellNavigatorKey = GlobalKey<NavigatorState>();
 
+  String _toQueryString(Map<String, String> query, [String? removeKey]) {
+    final newQuery = {...query};
+    if (removeKey != null) {
+      newQuery.remove(removeKey);
+    }
+    return Uri(queryParameters: newQuery).query;
+  }
+
+  String redirectToLoginPage(
+    BuildContext context,
+    GoRouterState state,
+    Map<String, String> query,
+  ) {
+    final queryString = _toQueryString(query);
+    final fromPage = state.subloc == '/' ? '' : '?from=${state.subloc}&$queryString';
+    return LoginRoute.path + fromPage;
+  }
+
+  String redirectToInitialPage(
+    BuildContext context,
+    GoRouterState state,
+    Map<String, String> query,
+  ) {
+    final queryString = _toQueryString(query, 'from');
+    return '${state.queryParams['from'] ?? '/'}?$queryString';
+  }
+
+  Future<String?> joinCampaign(
+    BuildContext context,
+    GoRouterState state,
+    Map<String, String> query,
+    int id,
+  ) async {
+    await context.read<GigaTurnipApiClient>().joinCampaign(id);
+    final queryString = _toQueryString(query, 'join_campaign');
+    return '${TaskRelevantRoute.path.replaceFirst(':cid', '$id')}/?$queryString';
+  }
+
   get router {
     return GoRouter(
       refreshListenable: _authRouterNotifier,
-      redirect: _authRouterNotifier.redirect,
+      redirect: (BuildContext context, GoRouterState state) async {
+        final authenticationService = context.read<AuthenticationRepository>();
+
+        final bool loggedIn = authenticationService.user.isNotEmpty;
+        final bool loggingIn = state.subloc == LoginRoute.path;
+
+        final query = {...state.queryParams};
+
+        // bundle the location the user is coming from into a query parameter
+        if (!loggedIn) return loggingIn ? null : redirectToLoginPage(context, state, query);
+
+        // if the user is logged in, send them where they were going before (or
+        // home if they weren't going anywhere)
+        if (loggingIn) return redirectToInitialPage(context, state, query);
+
+        // if there is query parameter <join_campaign>, then join campaign and send them to relevant task page
+        final campaignIdQueryValue = query['join_campaign'];
+
+        if (loggedIn && campaignIdQueryValue != null) {
+          try {
+            final campaignId = int.parse(campaignIdQueryValue);
+            return await joinCampaign(context, state, query, campaignId);
+          } on FormatException {
+            final queryString = _toQueryString(query, 'join_campaign');
+            return '${state.subloc}?$queryString';
+          }
+        }
+
+        // no need to redirect at all
+        return null;
+      },
       navigatorKey: _rootNavigatorKey,
       routes: [
         LoginRoute(parentKey: _rootNavigatorKey).route,
@@ -47,25 +117,5 @@ class RouterNotifier extends ChangeNotifier {
   void dispose() {
     _subscription.cancel();
     super.dispose();
-  }
-
-  String? redirect(BuildContext context, GoRouterState state) {
-    final bool loggedIn = _authenticationRepository.user.isNotEmpty;
-    final bool loggingIn = state.subloc == '/login';
-
-    // bundle the location the user is coming from into a query parameter
-    final query = {...state.queryParams};
-    var queryString = Uri(queryParameters: query).query;
-    final fromPage = state.subloc == '/' ? '' : '?from=${state.subloc}&$queryString';
-    if (!loggedIn) return loggingIn ? null : '/login$fromPage';
-
-    // if the user is logged in, send them where they were going before (or
-    // home if they weren't going anywhere)
-    query.remove('from');
-    queryString = Uri(queryParameters: query).query;
-    if (loggingIn) return '${state.queryParams['from'] ?? '/'}?$queryString';
-
-    // no need to redirect at all
-    return null;
   }
 }
