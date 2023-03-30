70b5e60b982433ea9c244b91170241aea7329590
atna <atay.narynov@gmail.com>
2023-03-30 17:32:51 +0600

    Add flutter_screenutil package for adaptive design

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gigaturnip/src/bloc/localization_bloc/localization_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class App extends StatelessWidget {
   final GoRouter router;
+
   const App({Key? key, required this.router}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationProvider: router.routeInformationProvider,
-      routeInformationParser: router.routeInformationParser,
-      routerDelegate: router.routerDelegate,
-      debugShowCheckedModeBanner: false,
-      // theme: web_theme,
-      locale: context.watch<LocalizationBloc>().state.locale,
-      supportedLocales: AppLocalizations.supportedLocales,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
+    return ScreenUtilInit(
+      designSize: const Size(428, 967),
+      builder: (context, child) {
+        return MaterialApp.router(
+          routeInformationProvider: router.routeInformationProvider,
+          routeInformationParser: router.routeInformationParser,
+          routerDelegate: router.routerDelegate,
+          debugShowCheckedModeBanner: false,
+          theme: theme,
+          locale: context.watch<LocalizationBloc>().state.locale,
+          supportedLocales: AppLocalizations.supportedLocales,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+        );
+      },
     );
   }
 }
+
+
+final theme = ThemeData(
+  primaryColor: const Color(0xff5E81FB),
+
+  fontFamily: 'Inter',
+
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+    titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+    bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+  ),
+);
