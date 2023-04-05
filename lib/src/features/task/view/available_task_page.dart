fe3df7fa178bc95b28e6a44b7006ab54b55ea749
atna <atay.narynov@gmail.com>
2023-04-05 14:37:43 +0600

    Replace ListViewWithPagination with CustomScrollView.

--- lib/src/features/task/view/available_task_page.dart
+++ lib/src/features/task/view/available_task_page.dart
@@ -7,6 +7,7 @@ import 'package:gigaturnip/src/widgets/widgets.dart';
 import 'package:gigaturnip_api/gigaturnip_api.dart' show GigaTurnipApiClient;
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 import '../bloc/bloc.dart';
 
@@ -75,33 +76,88 @@ class TaskView extends StatelessWidget {
           },
         ),
       ],
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListViewWithPagination<TaskStage, CreatableTaskCubit>(
-              itemBuilder: (context, index, item) {
-                return ListTile(
-                  title: Text(item.name),
-                  onTap: () {
-                    context.read<CreatableTaskCubit>().createTask(item);
-                  },
+      child: CustomScrollView(
+        slivers: [
+          BlocBuilder<CreatableTaskCubit, RemoteDataState<TaskStage>>(
+            builder: (context, state) {
+              if (state is RemoteDataLoading<TaskStage>) {
+                return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+              }
+              if (state is RemoteDataFailed<TaskStage>) {
+                return SliverToBoxAdapter(child: Center(child: Text(state.error)));
+              }
+              if (state is RemoteDataLoaded<TaskStage>) {
+                return MultiSliver(
+                  children: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final item = state.data[index];
+                          return ListTile(
+                            title: Text(item.name),
+                            onTap: () {
+                              context.read<CreatableTaskCubit>().createTask(item);
+                            },
+                          );
+                        },
+                        childCount: state.data.length,
+                      ),
+                    ),
+                    Pagination(
+                      currentPage: state.currentPage,
+                      total: state.total,
+                      onChanged: (page) => context.read<CreatableTaskCubit>().fetchData(page),
+                      enabled: state is! RemoteDataLoading,
+                    ),
+                  ],
                 );
-              },
-            ),
-            ListViewWithPagination<Task, AvailableTaskCubit>(
-              header: Text(context.loc.available_tasks),
-              itemBuilder: (context, index, item) {
-                return ListTile(
-                  title: Text(item.name),
-                  subtitle: Text(item.id.toString()),
-                  onTap: () {
-                    context.read<AvailableTaskCubit>().requestTaskAssignment(item);
-                  },
+              }
+              return const SliverToBoxAdapter(child: SizedBox.shrink());
+            },
+          ),
+          BlocBuilder<AvailableTaskCubit, RemoteDataState<Task>>(
+            builder: (context, state) {
+              if (state is RemoteDataLoading<Task>) {
+                return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+              }
+              if (state is RemoteDataFailed<Task>) {
+                return SliverToBoxAdapter(child: Center(child: Text(state.error)));
+              }
+              if (state is RemoteDataLoaded<Task>) {
+                return MultiSliver(
+                  children: [
+                    Text(
+                      context.loc.available_tasks,
+                      textAlign: TextAlign.center,
+                    ),
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                          final item = state.data[index];
+                          return ListTile(
+                            title: Text(item.name),
+                            subtitle: Text(item.id.toString()),
+                            onTap: () {
+                              context.read<AvailableTaskCubit>().requestTaskAssignment(item);
+                            },
+                          );
+                        },
+                        childCount: state.data.length,
+                      ),
+                    ),
+                    Pagination(
+                      currentPage: state.currentPage,
+                      total: state.total,
+                      onChanged: (page) => context.read<AvailableTaskCubit>().fetchData(page),
+                      enabled: state is! RemoteDataLoading,
+                    ),
+                  ],
                 );
-              },
-            ),
-          ],
-        ),
+              }
+              return const SliverToBoxAdapter(child: SizedBox.shrink());
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/src/features/task/view/relevant_task_page.dart
+++ lib/src/features/task/view/relevant_task_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gigaturnip/extensions/buildcontext/loc.dart';
+import 'package:gigaturnip/src/bloc/bloc.dart';
 import 'package:gigaturnip/src/router/routes/routes.dart';
 import 'package:gigaturnip/src/widgets/widgets.dart';
 import 'package:gigaturnip_api/gigaturnip_api.dart' show GigaTurnipApiClient;
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 import 'package:go_router/go_router.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 import '../bloc/bloc.dart';
 
@@ -60,31 +62,89 @@ class TaskView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ListViewWithPagination<Task, OpenTaskCubit>(
-            header: Text(context.loc.open_tasks),
-            itemBuilder: (context, index, item) {
-              return ListTile(
-                title: Text(item.name),
-                subtitle: Text("${item.id} | ${context.loc.complete}: ${item.complete}"),
-                onTap: () => redirectToTask(context, item),
+    return CustomScrollView(
+      slivers: [
+        BlocBuilder<OpenTaskCubit, RemoteDataState<Task>>(
+          builder: (context, state) {
+            if (state is RemoteDataLoading<Task>) {
+              return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+            }
+            if (state is RemoteDataFailed<Task>) {
+              return SliverToBoxAdapter(child: Center(child: Text(state.error)));
+            }
+            if (state is RemoteDataLoaded<Task>) {
+              return MultiSliver(
+                children: [
+                  Text(
+                    context.loc.open_tasks,
+                    textAlign: TextAlign.center,
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final item = state.data[index];
+                        return ListTile(
+                          title: Text(item.name),
+                          subtitle: Text("${item.id} | ${context.loc.complete}: ${item.complete}"),
+                          onTap: () => redirectToTask(context, item),
+                        );
+                      },
+                      childCount: state.data.length,
+                    ),
+                  ),
+                  Pagination(
+                    currentPage: state.currentPage,
+                    total: state.total,
+                    onChanged: (page) => context.read<OpenTaskCubit>().fetchData(page),
+                    enabled: state is! RemoteDataLoading,
+                  ),
+                ],
               );
-            },
-          ),
-          ListViewWithPagination<Task, ClosedTaskCubit>(
-            header: Text(context.loc.closed_tasks),
-            itemBuilder: (context, index, item) {
-              return ListTile(
-                title: Text(item.name),
-                subtitle: Text("${item.id} | ${context.loc.complete}: ${item.complete}"),
-                onTap: () => redirectToTask(context, item),
+            }
+            return const SliverToBoxAdapter(child: SizedBox.shrink());
+          },
+        ),
+        BlocBuilder<ClosedTaskCubit, RemoteDataState<Task>>(
+          builder: (context, state) {
+            if (state is RemoteDataLoading<Task>) {
+              return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+            }
+            if (state is RemoteDataFailed<Task>) {
+              return SliverToBoxAdapter(child: Center(child: Text(state.error)));
+            }
+            if (state is RemoteDataLoaded<Task>) {
+              return MultiSliver(
+                children: [
+                  Text(
+                    context.loc.closed_tasks,
+                    textAlign: TextAlign.center,
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final item = state.data[index];
+                        return ListTile(
+                          title: Text(item.name),
+                          subtitle: Text("${item.id} | ${context.loc.complete}: ${item.complete}"),
+                          onTap: () => redirectToTask(context, item),
+                        );
+                      },
+                      childCount: state.data.length,
+                    ),
+                  ),
+                  Pagination(
+                    currentPage: state.currentPage,
+                    total: state.total,
+                    onChanged: (page) => context.read<ClosedTaskCubit>().fetchData(page),
+                    enabled: state is! RemoteDataLoading,
+                  ),
+                ],
               );
-            },
-          ),
-        ],
-      ),
+            }
+            return const SliverToBoxAdapter(child: SizedBox.shrink());
+          },
+        ),
+      ],
     );
   }
 }
