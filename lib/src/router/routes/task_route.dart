65399a31aa3609334828844491678d82efe9d08c
atna <atay.narynov@gmail.com>
2023-04-13 13:44:06 +0600

    Add task creation and campaign join to taskRoute.

--- lib/src/router/routes/task_route.dart
+++ lib/src/router/routes/task_route.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gigaturnip/src/features/task/view/task_page.dart';
+import 'package:gigaturnip_api/gigaturnip_api.dart';
 import 'package:go_router/go_router.dart';
+
+import '../utilities.dart';
+import 'routes.dart';
 
 class TaskRoute {
   static String name = 'tasks';
@@ -11,11 +19,60 @@ class TaskRoute {
 
   TaskRoute({required this.parentKey});
 
+  Future<String?> createTask(BuildContext context, GoRouterState state) async {
+    final params = {...state.params};
+    final query = {...state.queryParams};
+    final queryString = toQueryString(query, 'create_task');
+
+    try {
+      final stageId = int.parse(query['create_task']!);
+      final task = await context.read<GigaTurnipApiClient>().createTaskFromStageId(stageId);
+      return '${TaskDetailRoute.path.replaceFirst(':cid', '${params['cid']}').replaceFirst(':tid', '${task.id}')}/?$queryString';
+    } on FormatException {
+      return '${state.subloc}?$queryString';
+    }
+  }
+
+  Future<String?> joinCampaign(BuildContext context, GoRouterState state) async {
+    final params = {...state.params};
+    final query = {...state.queryParams};
+    final queryString = toQueryString(query, 'join');
+
+    try {
+      final campaignId = int.parse(params['cid']!);
+      await context.read<GigaTurnipApiClient>().joinCampaign(campaignId);
+    } catch (e, c) {
+      if (kDebugMode) {
+        print(e);
+        print(c);
+      }
+    }
+    return '${state.subloc}/?$queryString';
+  }
+
+  FutureOr<String?> redirect(BuildContext context, GoRouterState state) async {
+    final query = {...state.queryParams};
+
+    final joinCampaignQueryValue = query['join']?.toLowerCase() == 'true';
+    final createTaskIdQueryValue = query['create_task'];
+
+    if (joinCampaignQueryValue) {
+      return await joinCampaign(context, state);
+    }
+
+    if (createTaskIdQueryValue != null) {
+      return await createTask(context, state);
+    }
+
+    return null;
+  }
+
   GoRoute get route {
     return GoRoute(
       parentNavigatorKey: parentKey,
       name: name,
       path: path,
+      redirect: redirect,
       builder: (BuildContext context, GoRouterState state) {
         final id = state.params['cid'] ?? '';
 
