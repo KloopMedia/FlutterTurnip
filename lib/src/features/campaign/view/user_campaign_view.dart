bfd61535329fe49d8ae2b7cd6b8f8b94bbe92e7f
Kunduz <talaybekovna.kunduz@gmail.com>
2023-04-14 13:06:00 +0600

    Add campaigns page widgets for desktop.

--- lib/src/features/campaign/view/user_campaign_view.dart
+++ lib/src/features/campaign/view/user_campaign_view.dart
@@ -7,6 +7,9 @@ import 'package:gigaturnip/src/widgets/widgets.dart';
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 import 'package:go_router/go_router.dart';
 
+import '../desktop/web_campaign_list_item_with_progress.dart';
+import '../desktop/sliver_grid_view_with_pagination.dart';
+
 class UserCampaignView extends StatelessWidget {
   const UserCampaignView({Key? key}) : super(key: key);
 
@@ -19,14 +22,27 @@ class UserCampaignView extends StatelessWidget {
       );
     }
 
-    return SliverListViewWithPagination<Campaign, UserCampaignCubit>(
-      padding: EdgeInsets.symmetric(vertical: 10.h),
+    /// mobile
+    // return SliverListViewWithPagination<Campaign, UserCampaignCubit>(
+    //   padding: EdgeInsets.symmetric(vertical: 10.h),
+    //   itemBuilder: (context, index, item) {
+    //     return CampaignListItemWithProgress(
+    //       data: item,
+    //       onTap: () => redirectToTaskMenu(context, item.id),
+    //     );
+    //   },
+    // );
+
+    /// desktop
+    return SliverGridViewWithPagination<Campaign, UserCampaignCubit>(
+      padding: EdgeInsets.symmetric(horizontal: 10.h, vertical: 10.h),
       itemBuilder: (context, index, item) {
-        return CampaignListItemWithProgress(
+        return WebCampaignListItemWithProgress(
           data: item,
           onTap: () => redirectToTaskMenu(context, item.id),
         );
       },
     );
+
   }
 }
