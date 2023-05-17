1a54e678bca3fa8b7f410004c5b2df7c408d4df7
atna <atay.narynov@gmail.com>
2023-05-17 13:47:31 +0600

    Refactor cards folder.

--- lib/src/widgets/card/index.dart
+++ lib/src/widgets/card/index.dart
@@ -1,7 +1,3 @@
 export 'base_card.dart';
 export 'card_with_chip_and_title.dart';
-export 'card_description.dart';
-export 'card_message.dart';
-export 'card_date.dart';
-export 'card_chip.dart';
-export 'status_card_chip.dart';
+export 'addons/index.dart';
