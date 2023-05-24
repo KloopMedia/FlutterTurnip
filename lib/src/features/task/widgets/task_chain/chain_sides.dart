a6bcaa7cd0a17f2b10a615629a99427119718750
atna <atay.narynov@gmail.com>
2023-05-24 20:40:21 +0600

    Refactor chain side.

--- lib/src/features/task/widgets/task_chain/chain_sides.dart
+++ lib/src/features/task/widgets/task_chain/chain_sides.dart
@@ -4,58 +4,41 @@ import 'chain_lines.dart';
 
 class ChainSide extends StatelessWidget {
   final PaintStyle style;
-  final bool even;
+  final bool isEven;
 
   const ChainSide({
     Key? key,
     required this.style,
-    required this.even,
+    required this.isEven,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (even) {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          const SizedBox(width: 60.0),
-          Expanded(
-            child: CustomPaint(
-              size: const Size(0.0, 0.0),
-              painter: StraightLine(style: style),
-            ),
+    final alignment = isEven ? Alignment.centerRight : Alignment.centerLeft;
+    final painter = isEven ? CurveLine.right(style: style) : CurveLine.left(style: style);
+
+    const curveWidth = 50.0;
+    const offset = curveWidth + 5;
+
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Positioned(
+          left: offset,
+          right: offset,
+          child: CustomPaint(
+            size: const Size(double.infinity, 0.0),
+            painter: StraightLine(style: style),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: CustomPaint(
-              size: const Size(50, 120),
-              painter: CurveLine.right(style: style),
-            ),
+        ),
+        Align(
+          alignment: alignment,
+          child: CustomPaint(
+            size: const Size(curveWidth, 120),
+            painter: painter,
           ),
-        ],
-      );
-    } else {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: CustomPaint(
-              size: const Size(50, 120),
-              painter: CurveLine.left(style: style),
-            ),
-          ),
-          Expanded(
-            child: CustomPaint(
-              size: const Size(0.0, 0.0),
-              painter: StraightLine(style: style),
-            ),
-          ),
-          const SizedBox(width: 60.0),
-        ],
-      );
-    }
+        ),
+      ],
+    );
   }
 }
