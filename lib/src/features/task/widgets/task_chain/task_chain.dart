f1f85fb6b993967c1571391f27c16bc05ed8298c
atna <atay.narynov@gmail.com>
2023-05-25 15:13:46 +0600

    Refactor lesson icon.

--- lib/src/features/task/widgets/task_chain/task_chain.dart
+++ lib/src/features/task/widgets/task_chain/task_chain.dart
@@ -1,159 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:gigaturnip/src/features/task/widgets/task_chain/task_stage_chain_page.dart';
 import 'package:gigaturnip/src/theme/index.dart';
-import 'chain_sides.dart';
-
-class TaskStageChain extends StatelessWidget {
-  final String title;
-  final ChainInfoStatus status;
-  final int lessonNum;
-  final bool even;
-  final bool isFirstTaskNotOpen;
-  final Color lineColor;
-  final void Function() onTap;
-
-  const TaskStageChain({
-    Key? key,
-    required this.title,
-    required this.status,
-    required this.lessonNum,
-    required this.even,
-    this.isFirstTaskNotOpen = false,
-    required this.lineColor,
-    required this.onTap,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context).colorScheme;
-    final titleTextStyle = TextStyle(
-      fontWeight: FontWeight.w500,
-      fontSize: 16.0,
-      color: theme.isLight
-          ? (status == ChainInfoStatus.notStarted) ? theme.neutral90 : theme.neutral40
-          : (status == ChainInfoStatus.notStarted) ? theme.neutralVariant40 : theme.neutral70,
-    );
-
-    return SizedBox(
-      child: Stack(
-        alignment: AlignmentDirectional.center,
-        children: [
-          ChainSide(color: lineColor, even: even),
-          SizedBox(
-            height: 90.0,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                children: [
-                  if (!even) LessonIcon(
-                    lessonNum: lessonNum,
-                    status: status,
-                    isFirstTaskNotOpen: isFirstTaskNotOpen,
-                  ),
-                  Expanded(
-                      child: TextButton(
-                        onPressed: (status == 'Неотправлено') ? null : onTap,
-                        child: Text(
-                          title,
-                          style: titleTextStyle,
-                          textAlign: even ? TextAlign.end : TextAlign.start,
-                        ),
-                      ),
-                    ),
-                  if (even) LessonIcon(
-                    lessonNum: lessonNum,
-                    status: status,
-                    isFirstTaskNotOpen: isFirstTaskNotOpen,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 class LessonIcon extends StatelessWidget {
   final int lessonNum;
   final ChainInfoStatus status;
-  final bool isFirstTaskNotOpen;
 
   const LessonIcon({
     Key? key,
     required this.lessonNum,
     required this.status,
-    required this.isFirstTaskNotOpen,
   }) : super(key: key);
-
-  int getStatusIndexOfIconColor() {
-    if (status ==  ChainInfoStatus.complete) {
-      return 0;
-    } else if (status == ChainInfoStatus.active){
-      return 1;
-    } else {
-      return 2;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
+
     final lessonNumTextStyle = TextStyle(
       fontWeight: FontWeight.w500,
       fontSize: 20.0,
-      color: theme.isLight
-          ? theme.onPrimary
-          : (status == ChainInfoStatus.notStarted) ? theme.neutralVariant40 : theme.neutral100,
+      color: status == ChainInfoStatus.notStarted ? theme.neutralVariant40 : Colors.white,
     );
-    final index = getStatusIndexOfIconColor();
-    final circleColorList = [
-      [const Color(0xFF94A9F0), const Color(0xFFC0CEFF)],
-      [const Color(0xFFBFAE0E), const Color(0xFFDFC902), const Color(0xFFDFC902)],
-      theme.isLight ? [const Color(0xFFE1E3E3),const Color(0xFFE1E3E3)] : [const Color(0xFF5D5E67), const Color(0xFF5D5E67)]/*variant40*/
-    ];
-    final rombColorList = [
-      const Color(0xFF748AD9),
-      const Color(0xFFEAD620),
-      theme.isLight ? const Color(0xFFEFF0F0) : const Color(0xFF767680)
-    ];
+
+    final circleColorSchema = {
+      ChainInfoStatus.complete: [
+        const Color(0xFFC0CEFF),
+        const Color(0xFF94A9F0),
+      ],
+      ChainInfoStatus.active: [
+        const Color(0xFFDFC902),
+        const Color(0xFFDFC902),
+        const Color(0xFFBFAE0E)
+      ],
+      ChainInfoStatus.notStarted: theme.isLight
+          ? [const Color(0xFFE1E3E3), const Color(0xFFE1E3E3)]
+          : [const Color(0xFF5D5E67), const Color(0xFF5D5E67)]
+    };
+
+    final rhombusColorSchema = {
+      ChainInfoStatus.complete: const Color(0xFF748AD9),
+      ChainInfoStatus.active: const Color(0xFFEAD620),
+      ChainInfoStatus.notStarted: theme.isLight ? const Color(0xFFEFF0F0) : const Color(0xFF767680)
+    };
 
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 15.0),
       width: 60.0,
       height: 60.0,
-      decoration:
-      BoxDecoration(
+      decoration: BoxDecoration(
           shape: BoxShape.circle,
           gradient: LinearGradient(
             begin: Alignment.bottomRight,
             end: Alignment.topLeft,
-            colors: circleColorList[index],
-          )
-      ),
-      child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Transform.rotate(
-              angle: 0.8,
-              child: Container(
-                height: 37.0,
-                width: 37.0,
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-                  color: rombColorList[index],
-                 ),
-              ),
+            colors: circleColorSchema[status]!,
+          )),
+      child: Stack(alignment: Alignment.center, children: [
+        Transform.rotate(
+          angle: 0.8,
+          child: Container(
+            height: 37.0,
+            width: 37.0,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+              color: rhombusColorSchema[status],
             ),
-            Center(
-              child: Text(
-                '$lessonNum',
-                style: lessonNumTextStyle,
-              ),
-            ),
-          ]
-      ),
+          ),
+        ),
+        Center(
+          child: Text(
+            '$lessonNum',
+            style: lessonNumTextStyle,
+          ),
+        ),
+      ]),
     );
   }
 }
