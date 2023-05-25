f5384fa97d5a829203b535ca105f561b51c338b7
atna <atay.narynov@gmail.com>
2023-05-25 14:55:07 +0600

    Refactor chain page. Add offset to curve lines to fit InkWell.

--- lib/src/features/task/widgets/task_chain/chain_sides.dart
+++ lib/src/features/task/widgets/task_chain/chain_sides.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:gigaturnip/src/features/task/widgets/task_chain/task_stage_chain_page.dart';
 
 import 'chain_lines.dart';
 
 class ChainSide extends StatelessWidget {
   final PaintStyle style;
   final bool isEven;
+  final ChainPosition position;
 
   const ChainSide({
     Key? key,
     required this.style,
     required this.isEven,
+    required this.position,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final alignment = isEven ? Alignment.centerRight : Alignment.centerLeft;
-    final painter = isEven ? CurveLine.right(style: style) : CurveLine.left(style: style);
+    final curveLine = isEven ? CurveLine.right(style: style) : CurveLine.left(style: style);
 
     const curveWidth = 50.0;
-    const offset = curveWidth + 5;
+    const offset = curveWidth / 2 + 10;
+
+    final startOffset = position == ChainPosition.start ? offset + 25 : offset;
+    final endOffset = position == ChainPosition.end ? offset + 25 : offset;
 
     return Stack(
       alignment: Alignment.topCenter,
       children: [
         Positioned(
-          left: offset,
+          left: startOffset,
           right: offset,
           child: CustomPaint(
             size: const Size(double.infinity, 0.0),
@@ -35,9 +41,19 @@ class ChainSide extends StatelessWidget {
           alignment: alignment,
           child: CustomPaint(
             size: const Size(curveWidth, 120),
-            painter: painter,
+            painter: curveLine,
           ),
         ),
+        if (position == ChainPosition.end)
+          Positioned(
+            bottom: 0,
+            left: isEven ? endOffset : offset,
+            right: isEven ? offset : endOffset,
+            child: CustomPaint(
+              size: const Size(double.infinity, 0.0),
+              painter: StraightLine(style: style),
+            ),
+          ),
       ],
     );
   }
--- lib/src/features/task/widgets/task_chain/task_stage_chain_page.dart
+++ lib/src/features/task/widgets/task_chain/task_stage_chain_page.dart
@@ -6,6 +6,7 @@ import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 
 import '../../../../bloc/bloc.dart';
 import 'chain_lines.dart';
+import 'chain_sides.dart';
 import 'task_chain.dart';
 
 enum ChainInfoStatus {
@@ -31,129 +32,174 @@ class TaskStageChainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context).colorScheme;
-    Widget leftStarIcon = Row(
-      children: [
-        Icon(
-          Icons.star,
-          color: theme.isLight ? const Color(0xFFE1E3E3) : theme.neutralVariant40,
-          size: 50.0,
-        ),
-        const Spacer()
-      ],
-    );
-
-    Widget rightStarIcon = Row(
-      children: [
-        const Spacer(),
-        Icon(
-          Icons.star,
-          color: theme.isLight ? const Color(0xFFE1E3E3) : theme.neutralVariant40,
-          size: 50.0,
-        )
-      ],
-    );
-
     return BlocBuilder<IndividualChainCubit, RemoteDataState<IndividualChain>>(
       builder: (context, state) {
-        if (state is RemoteDataLoaded<IndividualChain> && state.data.isNotEmpty) {
+        if (state is RemoteDataInitialized<IndividualChain> && state.data.isNotEmpty) {
           final individualChains = state.data;
           List<Widget> tasks = [];
           for (var chain in individualChains) {
             tasks.add(Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 20,
-                horizontal:
-                    context.isSmall || context.isMedium ? 5 : MediaQuery.of(context).size.width / 6,
-              ),
+              margin: const EdgeInsets.symmetric(vertical: 120, horizontal: 20),
               child: ListView.builder(
                 shrinkWrap: true,
+                itemCount: chain.stagesData.length,
                 itemBuilder: (context, index) {
                   final item = chain.stagesData[index];
                   final status = getTaskStatus(item);
-                  final lineColor = (status == ChainInfoStatus.complete)
-                      ? theme.isLight
-                          ? const Color(0xFF2754F3)
-                          : const Color(0xFF7694FF)
-                      : theme.isLight
-                          ? const Color(0xFFE1E3E3)
-                          : theme.neutral20;
 
+                  ChainPosition position;
                   if (index == 0) {
-                    return Stack(
-                      alignment: Alignment.topCenter,
-                      children: [
-                        Container(
-                          alignment: Alignment.bottomCenter,
-                          height: 150.0,
-                          child: TaskStageChain(
-                            title: item.name,
-                            status: status,
-                            lessonNum: index + 1,
-                            even: index % 2 == 0 ? true : false,
-                            lineColor: lineColor,
-                            onTap: () => onTap(item, status),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5.0, bottom: 5.0),
-                              child: Image.asset('assets/images/flag.png', height: 50.0),
-                            ),
-                            Expanded(
-                              child: CustomPaint(
-                                size: const Size(0.0, 0.0),
-                                painter: StraightLine(
-                                    color: lineColor,
-                                    dashWidth: context.isSmall ? 10.0 : 16.0,
-                                    dashSpace: context.isSmall ? 10.0 : 12.0,
-                                    strokeWidth: context.isSmall ? 6.0 : 7.0),
-                              ),
-                            ),
-                            const SizedBox(width: 60.0),
-                          ],
-                        )
-                      ],
-                    );
-                  } else if (chain.stagesData.length == index + 1) {
-                    return Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: [
-                        Container(
-                          alignment: Alignment.topCenter,
-                          height: 150.0,
-                          child: TaskStageChain(
-                            title: item.name,
-                            status: status,
-                            lessonNum: index + 1,
-                            even: index % 2 == 0 ? true : false,
-                            lineColor: lineColor,
-                            onTap: () => onTap(item, status),
-                          ),
-                        ),
-                        if (index % 2 == 0) leftStarIcon,
-                        if (index % 2 != 0) rightStarIcon,
-                      ],
-                    );
+                    position = ChainPosition.start;
+                  } else if (index == chain.stagesData.length - 1) {
+                    position = ChainPosition.end;
                   } else {
-                    return TaskStageChain(
-                      title: item.name,
-                      status: status,
-                      lessonNum: index + 1,
-                      even: index % 2 == 0 ? true : false,
-                      lineColor: lineColor,
-                      onTap: () => onTap(item, status),
-                    );
+                    position = ChainPosition.middle;
                   }
+
+                  return ChainRow(
+                    position: position,
+                    title: item.name,
+                    index: index,
+                    status: status,
+                    onTap: () => onTap(item, status),
+                  );
                 },
-                itemCount: chain.stagesData.length,
               ),
             ));
           }
           return SliverToBoxAdapter(child: Column(children: tasks));
         }
         return const SliverToBoxAdapter(child: SizedBox.shrink());
+      },
+    );
+  }
+}
+
+class Position {
+  final double? top;
+  final double? bottom;
+  final double? left;
+  final double? right;
+
+  const Position({this.top, this.bottom, this.left, this.right});
+}
+
+enum ChainPosition { start, middle, end }
+
+class ChainRow extends StatelessWidget {
+  final int index;
+  final String title;
+  final ChainInfoStatus status;
+  final ChainPosition position;
+  final void Function()? onTap;
+
+  const ChainRow({
+    Key? key,
+    required this.title,
+    required this.status,
+    required this.index,
+    required this.position,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    final isEven = index % 2 == 0;
+
+    final completeLineColor = theme.isLight ? const Color(0xFF2754F3) : const Color(0xFF7694FF);
+    final notStartedLineColor = theme.isLight ? const Color(0xFFE1E3E3) : theme.neutral20;
+
+    final lineColor =
+        (status == ChainInfoStatus.complete) ? completeLineColor : notStartedLineColor;
+
+    final dashWidth = context.isSmall ? 10.0 : 16.0;
+    final dashSpace = context.isSmall ? 10.0 : 12.0;
+    final strokeWidth = context.isSmall ? 5.0 : 7.0;
+
+    final style = PaintStyle(
+      color: lineColor,
+      dashWidth: dashWidth,
+      dashSpace: dashSpace,
+      strokeWidth: strokeWidth,
+    );
+
+    final notStartedColor = theme.isLight ? theme.neutral90 : theme.neutralVariant40;
+    final activeColor = theme.isLight ? theme.neutral40 : theme.neutral70;
+
+    final titleTextStyle = TextStyle(
+      fontWeight: FontWeight.w500,
+      fontSize: 16.0,
+      color: status == ChainInfoStatus.notStarted ? notStartedColor : activeColor,
+    );
+
+    return InkWell(
+      onTap: onTap,
+      borderRadius: const BorderRadius.all(Radius.elliptical(50, 80)),
+      child: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: titleTextStyle,
+              textAlign: isEven ? TextAlign.right : TextAlign.left,
+            ),
+          ),
+          Align(
+            alignment: isEven ? Alignment.centerRight : Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: LessonIcon(
+                lessonNum: index,
+                status: status,
+              ),
+            ),
+          ),
+          ChainSide(
+            style: style,
+            isEven: isEven,
+            position: position,
+          ),
+          ChainRowIcon(position: position, isEven: isEven),
+        ],
+      ),
+    );
+  }
+}
+
+class ChainRowIcon extends StatelessWidget {
+  final ChainPosition position;
+  final bool isEven;
+
+  const ChainRowIcon({Key? key, required this.position, required this.isEven}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    Position? iconPosition;
+    if (position == ChainPosition.start) {
+      iconPosition = const Position(left: 0, top: -25);
+    } else if (position == ChainPosition.end) {
+      iconPosition =
+          isEven ? const Position(left: 0, bottom: -25) : const Position(right: 0, bottom: -25);
+    }
+
+    final color = theme.isLight ? const Color(0xFFE1E3E3) : theme.neutralVariant40;
+
+    return Positioned(
+      top: iconPosition?.top,
+      bottom: iconPosition?.bottom,
+      left: iconPosition?.left,
+      right: iconPosition?.right,
+      child: switch (position) {
+        ChainPosition.start => Image.asset('assets/images/flag.png', height: 50.0),
+        ChainPosition.end => Icon(Icons.star, color: color, size: 50.0),
+        ChainPosition.middle => const SizedBox.shrink(),
       },
     );
   }
