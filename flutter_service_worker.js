b8f97073ca8de0e8159747b0a4be53c709f4bc4f
atna <atay.narynov@gmail.com>
2023-08-23 17:13:42 +0600

    Built web
    Branch: development
    Commit: 814930969cdcacd4756c5feb69940037058dede8
    
    package:peanut 4.3.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"version.json": "7cb6435b5c0978eea0af0b27865de495",
-"index.html": "0386c87bf1c64c90cdb76ae6c23fcc5f",
-"/": "0386c87bf1c64c90cdb76ae6c23fcc5f",
-"main.dart.js": "cce973baadf1f6fed50a3dbd7c61afff",
+const RESOURCES = {"version.json": "d39ea862bd5b6c625502e955946619fd",
+"index.html": "aa9afec6de0714b4448dc87f0ceebf7e",
+"/": "aa9afec6de0714b4448dc87f0ceebf7e",
+"main.dart.js": "80e2540e033e4ad0ffc657553d6d7e2b",
 ".well-known/assetlinks.json": "545f3866eec916ef6b3e765aa43abb53",
 "sqlite3.wasm": "2068781fd3a05f89e76131a98da09b5b",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
