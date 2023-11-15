77e51d38727622238f80c07c33fb609045ac4f2a
atna <atay.narynov@gmail.com>
2023-11-15 17:00:44 +0600

    Add chain repository.

--- packages/gigaturnip_repository/lib/src/repositories/chain_repository.dart
+++ packages/gigaturnip_repository/lib/src/repositories/chain_repository.dart
@@ -1,6 +1,30 @@
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 import 'package:gigaturnip_api/gigaturnip_api.dart' as api;
 
+class ChainRepository extends GigaTurnipRepository<Chain> {
+  final api.GigaTurnipApiClient _gigaTurnipApiClient;
+  final int campaignId;
+
+  ChainRepository({
+    required api.GigaTurnipApiClient gigaTurnipApiClient,
+    required this.campaignId,
+  }) : _gigaTurnipApiClient = gigaTurnipApiClient;
+
+  @override
+  Future<api.PaginationWrapper<Chain>> fetchAndParseData({Map<String, dynamic>? query}) async {
+    final data = await _gigaTurnipApiClient.getChains(query: {
+      'campaign': campaignId,
+      ...?query,
+    });
+
+    return data.copyWith<Chain>(count: data.count, results: parseData(data.results));
+  }
+
+  List<Chain> parseData(List<api.Chain> data) {
+    return data.map(Chain.fromApiModel).toList();
+  }
+}
+
 class IndividualChainRepository extends GigaTurnipRepository<IndividualChain> {
   final api.GigaTurnipApiClient _gigaTurnipApiClient;
   final int campaignId;
