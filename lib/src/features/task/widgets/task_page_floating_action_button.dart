c8cbddf2dfa6c9e7ef90044f111652dca88482f3
atna <atay.narynov@gmail.com>
2023-11-16 15:17:54 +0600

    Refactor TaskPageFloatingActionButton.

--- lib/src/features/task/widgets/task_page_floating_action_button.dart
+++ lib/src/features/task/widgets/task_page_floating_action_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gigaturnip/src/bloc/bloc.dart';
+import 'package:gigaturnip/src/widgets/button/custom_floating_action_button.dart';
 import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 
 import '../bloc/bloc.dart';
@@ -13,29 +14,21 @@ class TaskPageFloatingActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context).colorScheme;
-
     return BlocBuilder<ProactiveTasks, RemoteDataState<TaskStage>>(
       builder: (context, state) {
         if (state is RemoteDataInitialized<TaskStage> && state.data.isNotEmpty) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 10, right: 8),
-            child: FloatingActionButton(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-              backgroundColor: theme.primary,
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (_) {
-                    return BlocProvider.value(
-                      value: context.read<ProactiveTasks>(),
-                      child: CreatableTaskMenu(campaignId: campaignId),
-                    );
-                  },
-                );
-              },
-              child: const Icon(Icons.add),
-            ),
+          return CustomFloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return BlocProvider.value(
+                    value: context.read<ProactiveTasks>(),
+                    child: CreatableTaskMenu(campaignId: campaignId),
+                  );
+                },
+              );
+            },
           );
         }
         return const SizedBox.shrink();
---
+++ lib/src/widgets/button/custom_floating_action_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final EdgeInsets? padding;
+  final void Function() onPressed;
+
+  const CustomFloatingActionButton({super.key, required this.onPressed, this.padding});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: padding ?? const EdgeInsets.only(bottom: 10, right: 8),
+      child: FloatingActionButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        backgroundColor: theme.primary,
+        onPressed: onPressed,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
