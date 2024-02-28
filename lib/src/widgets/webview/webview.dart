3407eb9410c36e5c271a7611982b740e3548bf19
Kunduz <talaybekovna.kunduz@gmail.com>
2024-02-28 13:20:32 +0600

    Fix colored text is black.

--- lib/src/widgets/webview/webview.dart
+++ lib/src/widgets/webview/webview.dart
@@ -58,7 +58,7 @@ class _WebViewState extends State<WebView> {
       #spacer {
         padding: 8px 16px;
       }
-      body, span, p, h1, h2, h3, h4, h5 {
+      p, h1, h2, h3, h4, h5, em, li {
         color: $backgroundColor !important;
       }
       </style>  
