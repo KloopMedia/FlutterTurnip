86ee0b8a7ff6dd9eecc3d4664baf7fbc726b39f2
atna <atay.narynov@gmail.com>
2024-04-09 12:41:46 +0600

    Add translation for empty notifications.

--- lib/src/features/notification/widgets/no_items_found_indicator.dart
+++ lib/src/features/notification/widgets/no_items_found_indicator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gigaturnip/extensions/buildcontext/loc.dart';
 
 import 'first_page_exception_indicator.dart';
 
@@ -7,8 +8,7 @@ class NoItemsFoundIndicator extends StatelessWidget {
   const NoItemsFoundIndicator({super.key});
 
   @override
-  Widget build(BuildContext context) => const FirstPageExceptionIndicator(
-        title: 'No items found',
-        message: 'The list is currently empty.',
+  Widget build(BuildContext context) => FirstPageExceptionIndicator(
+        title: context.loc.no_notifications,
       );
 }
