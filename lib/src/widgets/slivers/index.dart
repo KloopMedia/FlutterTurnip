f0d119f6eb1ada4ad0317e7939504df947752cd3
atna <atay.narynov@gmail.com>
2024-12-20 15:20:33 +0600

    Add SliverGridView

--- lib/src/widgets/slivers/index.dart
+++ lib/src/widgets/slivers/index.dart
@@ -3,3 +3,4 @@ export 'list_view_with_pagination.dart';
 export 'sliver_list_view_with_pagination.dart';
 export 'sliver_grid_view_with_pagination.dart';
 export 'adaptive_list_view.dart';
+export 'sliver_grid_view.dart';
