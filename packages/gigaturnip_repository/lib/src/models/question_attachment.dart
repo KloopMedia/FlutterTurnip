ab6d5c7543f93f65920f90567cc0392ce6f7ff64
atna <atay.narynov@gmail.com>
2025-01-15 17:11:44 +0600

    Add QuestionAttachmentType

--- packages/gigaturnip_repository/lib/src/models/question_attachment.dart
+++ packages/gigaturnip_repository/lib/src/models/question_attachment.dart
@@ -3,10 +3,21 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'question_attachment.g.dart';
 
+enum QuestionAttachmentType {
+  audio,
+  image,
+  video,
+  other,
+}
+
 @JsonSerializable(explicitToJson: true)
 class QuestionAttachment {
   final int id;
-  final String type;
+  @JsonKey(
+    fromJson: _stringToType,
+    toJson: _stringFromType,
+  )
+  final QuestionAttachmentType type;
   final String file;
 
   QuestionAttachment({
@@ -19,8 +30,40 @@ class QuestionAttachment {
       _$QuestionAttachmentFromJson(json);
 
   factory QuestionAttachment.fromApiModel(api.QuestionAttachment model) {
-    return QuestionAttachment(id: model.id, type: model.type, file: model.file);
+    return QuestionAttachment(id: model.id, type: typeFromJson(model.type), file: model.file);
   }
 
   Map<String, dynamic> toJson() => _$QuestionAttachmentToJson(this);
+
+  static QuestionAttachmentType _stringToType(String json) => typeFromJson(json);
+
+  static String _stringFromType(QuestionAttachmentType json) => typeToJson(json);
+
+  static QuestionAttachmentType typeFromJson(String value) {
+    switch (value) {
+      case 'AUDIO':
+        return QuestionAttachmentType.audio;
+      case 'IMAGE':
+        return QuestionAttachmentType.image;
+      case 'VIDEO':
+        return QuestionAttachmentType.video;
+      case 'OTHER':
+        return QuestionAttachmentType.other;
+      default:
+        throw ArgumentError('Invalid QuestionAttachmentType: $value');
+    }
+  }
+
+  static String typeToJson(QuestionAttachmentType type) {
+    switch (type) {
+      case QuestionAttachmentType.audio:
+        return 'AUDIO';
+      case QuestionAttachmentType.image:
+        return 'IMAGE';
+      case QuestionAttachmentType.video:
+        return 'VIDEO';
+      case QuestionAttachmentType.other:
+        return 'OTHER';
+    }
+  }
 }
