d95b3186e1551adaec93212df0288f3d5fa5634b
atna <atay.narynov@gmail.com>
2025-01-17 18:34:37 +0600

    Refactor UserCampaignView

---
+++ lib/src/features/campaign/widgets/user_campaign_card.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:gigaturnip/extensions/buildcontext/loc.dart';
+import 'package:gigaturnip_repository/gigaturnip_repository.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+
+import '../../../router/routes/routes.dart';
+import '../../../widgets/card/addons/card_chip.dart';
+
+class UserCampaignCard extends StatelessWidget {
+  final Campaign campaign;
+
+  const UserCampaignCard({
+    super.key,
+    required this.campaign,
+  });
+
+  void _redirectToTaskMenu(BuildContext context, Campaign campaign) {
+    context.pushNamed(
+      TaskRoute.name,
+      pathParameters: {'cid': '${campaign.id}'},
+      extra: campaign,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final hasImage = campaign.featuredImage != null && campaign.featuredImage!.isNotEmpty;
+
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          // Shadow 1
+          BoxShadow(
+            color: Color(0x1A454545), // '#4545451A' in Flutter ARGB format
+            offset: Offset(0, 1), // Horizontal and vertical offset
+            blurRadius: 3, // Blur radius
+            spreadRadius: 0, // Spread radius
+          ),
+          // Shadow 2
+          BoxShadow(
+            color: Color(0x1A454545), // '#4545451A' in Flutter ARGB format
+            offset: Offset(0, 4), // Horizontal and vertical offset
+            blurRadius: 8, // Blur radius
+            spreadRadius: 3, // Spread radius
+          ),
+        ],
+      ),
+      child: InkWell(
+        onTap: () => _redirectToTaskMenu(context, campaign),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        campaign.name,
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        campaign.description,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    ],
+                  ),
+                ),
+                if (hasImage)
+                  Row(
+                    children: [
+                      SizedBox(width: 8),
+                      Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Image.network(
+                              campaign.featuredImage!,
+                              height: 54,
+                              width: 54,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+              ],
+            ),
+             if (campaign.startDate != null || campaign.isCompleted) Row(
+              children: [Column(
+                children: [
+                  SizedBox(height: 8),
+                  _CampaignStatusChip(campaign: campaign),
+                ],
+              )],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CampaignStatusChip extends StatelessWidget {
+  final Campaign campaign;
+
+  const _CampaignStatusChip({required this.campaign});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget? chip;
+
+    if (campaign.startDate != null) {
+      final startDateString = DateFormat.MMMMd(context.loc.localeName).format(campaign.startDate!);
+      chip = CardChip(
+        context.loc.course_start_at(startDateString),
+        fontColor: Colors.white,
+        backgroundColor: const Color(0xFF778CE0),
+      );
+    } else if (campaign.isCompleted) {
+      chip = CardChip(
+        context.loc.course_is_completed,
+        fontColor: Colors.white,
+        backgroundColor: const Color(0xFF74BF3B),
+      );
+    }
+
+    if (chip == null) return const SizedBox.shrink();
+
+    return Row(children: [chip]);
+  }
+}
