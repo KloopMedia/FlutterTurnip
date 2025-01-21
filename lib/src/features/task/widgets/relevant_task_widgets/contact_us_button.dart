50a0d9e8319683ac33816a6b688f248145bb9e26
atna <atay.narynov@gmail.com>
2025-01-21 13:28:14 +0600

    Update font weight and add new text

--- lib/src/features/task/widgets/relevant_task_widgets/contact_us_button.dart
+++ lib/src/features/task/widgets/relevant_task_widgets/contact_us_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gigaturnip/extensions/buildcontext/loc.dart';
 import 'package:gigaturnip/src/features/campaign_detail/bloc/campaign_detail_bloc.dart';
-import 'package:gigaturnip/src/theme/index.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactUsButton extends StatelessWidget {
@@ -14,7 +13,7 @@ class ContactUsButton extends StatelessWidget {
       builder: (context, state) {
         if (state is CampaignInitialized && state.data.contactUsLink != null) {
           return Padding(
-            padding: EdgeInsets.only(top: 24, bottom: 8, left: 16, right: 16),
+            padding: EdgeInsets.only(bottom: 20, left: 16, right: 16),
             child: InkWell(
               borderRadius: BorderRadius.circular(12),
               onTap: () async {
@@ -44,7 +43,7 @@ class ContactUsButton extends StatelessWidget {
                         Text(
                           context.loc.have_questions_campaign,
                           style: TextStyle(
-                            fontWeight: FontWeight.w500,
+                            fontWeight: FontWeight.w600,
                             fontSize: 16,
                             color: Colors.black,
                           ),
@@ -53,8 +52,8 @@ class ContactUsButton extends StatelessWidget {
                         Text(
                           context.loc.call_us_campaign,
                           style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 14,
                             color: Theme.of(context).colorScheme.primary,
                           ),
                         ),
@@ -65,6 +64,16 @@ class ContactUsButton extends StatelessWidget {
                         ),
                       ],
                     ),
+                    SizedBox(height: 2),
+                    Row(
+                      children: [
+                        SizedBox(width: 5),
+                        Text(
+                          context.loc.call_us_description,
+                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
