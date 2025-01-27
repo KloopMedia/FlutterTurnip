2fd816c7fdc721d6b950bbcd56fde6298fc033a6
atna <atay.narynov@gmail.com>
2025-01-27 16:04:37 +0600

    Extract chain building into utils.dart

--- lib/src/features/task/view/lesson_task_view.dart
+++ lib/src/features/task/view/lesson_task_view.dart
@@ -4,113 +4,13 @@ import 'package:gigaturnip_repository/gigaturnip_repository.dart';
 
 import '../../../bloc/bloc.dart';
 import '../bloc/bloc.dart';
+import '../utils.dart';
 import '../widgets/widgets.dart';
 
 class LessonTaskPage extends StatelessWidget {
   final Function(TaskStageChainInfo item, ChainInfoStatus status) onTap;
 
   const LessonTaskPage({super.key, required this.onTap});
-
-  /// Determines the status of a task stage chain.
-  ChainInfoStatus _getChainStatus(TaskStageChainInfo item) {
-    if (item.totalCount == 0) return ChainInfoStatus.notStarted;
-    if (item.completeCount > 0 && item.completeCount < item.totalCount) {
-      return ChainInfoStatus.returned;
-    }
-    if (item.completeCount < item.totalCount) return ChainInfoStatus.active;
-    return ChainInfoStatus.complete;
-  }
-
-  /// Builds the widget representation of a single chain.
-  List<Widget> _buildChain(List<TaskStageChainInfo> chainData) {
-    final List<Widget> chainWidgets = [];
-
-    for (int index = 0; index < chainData.length; index++) {
-      final currentItem = chainData[index];
-      final status = _getChainStatus(currentItem);
-      final isComplete = status == ChainInfoStatus.complete;
-      final isLast = index == (chainData.length - 1);
-
-      TaskStageChainInfo? nextItemPreview;
-      if (index < chainData.length - 1) {
-        final nextItem = chainData[index + 1];
-        final hasValidOutStages = nextItem.outStages.any((stage) => stage != null);
-
-        if (!hasValidOutStages && index + 1 < chainData.length - 1) {
-          nextItemPreview = nextItem;
-          index++; // Skip to next valid index.
-        }
-      }
-
-      chainWidgets.add(
-        _buildChainItem(
-          currentItem: currentItem,
-          status: status,
-          isComplete: isComplete,
-          isLast: isLast,
-          nextItemPreview: nextItemPreview,
-        ),
-      );
-    }
-
-    return chainWidgets;
-  }
-
-  /// Creates a widget for a single chain item.
-  Widget _buildChainItem({
-    required TaskStageChainInfo currentItem,
-    required ChainInfoStatus status,
-    required bool isComplete,
-    required bool isLast,
-    TaskStageChainInfo? nextItemPreview,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Row(
-        children: [
-          Stack(
-            children: [
-              LessonLine(isComplete: isComplete, isLast: isLast),
-              LessonDecorator(isComplete: isComplete),
-            ],
-          ),
-          const SizedBox(width: 10),
-          Expanded(
-            child: LessonListItem(
-              title: currentItem.name,
-              onTap: () => onTap(currentItem, status),
-            ),
-          ),
-          if (nextItemPreview != null) const SizedBox(width: 8),
-          if (nextItemPreview != null)
-            Expanded(
-              child: LessonListItem(
-                title: nextItemPreview.name,
-                onTap: () => onTap(nextItemPreview, status),
-              ),
-            ),
-        ],
-      ),
-    );
-  }
-
-  /// Builds the chains to be displayed in the UI.
-  List<Widget> _buildChains(List<IndividualChain> chainsData) {
-    return chainsData.expand<Widget>((chain) {
-      return [
-        const SizedBox(height: 10),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Text(
-            chain.name,
-            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 22.0),
-          ),
-        ),
-        ..._buildChain(chain.stagesData),
-        const SizedBox(height: 10),
-      ];
-    }).toList();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -129,7 +29,7 @@ class LessonTaskPage extends StatelessWidget {
         }
 
         if (state is RemoteDataInitialized<IndividualChain> && state.data.isNotEmpty) {
-          final chains = _buildChains(state.data);
+          final chains = buildChains(state.data, onTap);
           return SliverList.list(children: chains);
         }
 
