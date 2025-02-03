5d3015242693f7d49f52a335487b73511fb9d55a
atna <atay.narynov@gmail.com>
2025-02-03 10:23:41 +0600

    Turn off backbutton for web

--- lib/src/widgets/app_bar/new_scaffold_appbar.dart
+++ lib/src/widgets/app_bar/new_scaffold_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ScaffoldAppbar extends StatelessWidget {
@@ -42,6 +43,7 @@ class ScaffoldAppbar extends StatelessWidget {
             ),
           ),
         ),
+        automaticallyImplyLeading: !kIsWeb,
         titleSpacing: titleSpacing,
         leadingWidth: 64,
         leading: leading,
